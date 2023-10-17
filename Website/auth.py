cca3cddba08da43508a9dd4b5012f80d14dbf97d
Meher Vamsi Dontoju <75484855+Mehervamsi1@users.noreply.github.com>
2023-10-17 00:53:15 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   Website/__pycache__/auth.cpython-310.pyc
    	modified:   Website/auth.py
    	modified:   Website/templates/base.html
    	modified:   Website/templates/home.html
    	new file:   Website/templates/login.html
    	new file:   Website/templates/sign_up.html

--- Website/auth.py
+++ Website/auth.py
@@ -1,15 +1,34 @@
-from flask import Blueprint
-
+from flask import Blueprint, render_template, request,flash
+import re
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html",text='Testing', )
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    if request.method=='POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) <4:
+            flash('Email Name must be greater than 4 characters.', category='error')
+        elif re.fullmatch(regex, email):
+            flash('The email is not according to the Standard')
+        elif len(firstName)<2:
+            flash('First Name must be greater than 2 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match')
+        else:
+            #add user to the db 
+            pass
+    return render_template("sign_up.html") 
